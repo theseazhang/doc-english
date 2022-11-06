e0e9c069cec584dcc022211c95a78375ce5a172b
theseazhang <534929089@qq.com>
2022-11-06 14:17:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,129 @@
+import {
+  nextParse,
+  reactParse,
+  tailwindcssParse,
+  nodejsParse,
+  strapiParse,
+  sleep,
+} from "./lib/spider.js";
+import * as fs from "fs";
+
+function spiltStr(str) {
+  const res = [];
+  const tab = str.toLowerCase().split("\n");
+  for (const line of tab) {
+    for (let word of line.split(" ")) {
+      if (word.length < 2 || word.length > 26) continue;
+      if (/^[a-z]+$/.test(word)) res.push(word);
+    }
+  }
+
+  return res;
+}
+
+function wordCount(res) {
+  let tmp = new Map();
+  res.forEach((str) => {
+    let res = spiltStr(str);
+    res.forEach((i) => {
+      if (tmp.has(i)) tmp.set(i, tmp.get(i) + 1);
+      else tmp.set(i, 1);
+    });
+  });
+
+  let items = Array.from(tmp);
+  items.sort((a, b) => b[1] - a[1]);
+  return items;
+}
+
+async function runNext() {
+  const res = await nextParse();
+  const words = wordCount(res);
+  fs.writeFile("data/nextjs-words.json", JSON.stringify(words), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log(`next.js is done ! get ${words.length} words.`);
+}
+
+async function runReact() {
+  const res = await reactParse();
+  const words = wordCount(res);
+  fs.writeFile("data/reactjs-words.json", JSON.stringify(words), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log(`react.js is done ! get ${words.length} words.`);
+}
+
+async function runTailwindcss() {
+  const res = await tailwindcssParse();
+  const words = wordCount(res);
+  fs.writeFile("data/tailwindcss-words.json", JSON.stringify(words), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log(`Tailwind CSS is done ! get ${words.length} words.`);
+}
+
+async function runNodejs() {
+  const res = await nodejsParse();
+  const words = wordCount(res);
+  fs.writeFile("data/nodejs-words.json", JSON.stringify(words), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log(`Node.js is done ! get ${words.length} words.`);
+}
+
+async function runStrapi() {
+  const res = await strapiParse();
+  const words = wordCount(res);
+  fs.writeFile("data/strapi-words.json", JSON.stringify(words), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log(`Strapi is done ! get ${words.length} words.`);
+}
+
+const dictPath = [
+  "data/nextjs-words.json",
+  "data/reactjs-words.json",
+  "data/tailwindcss-words.json",
+  "data/nodejs-words.json",
+  "data/strapi-words.json",
+];
+
+function sortAllDicts() {
+  let tmp = new Map();
+  for (const path of dictPath) {
+    const res = JSON.parse(fs.readFileSync(path));
+    for (const i of res) {
+      let [k, v] = i;
+      if (tmp.has(k)) tmp.set(k, tmp.get(k) + v);
+      else tmp.set(k, v);
+    }
+  }
+
+  let items = Array.from(tmp);
+  items.sort((a, b) => b[1] - a[1]);
+  fs.writeFileSync("data/front-end-words.json", JSON.stringify(items));
+  console.log(`All front-end docs is done ! get ${items.length} words.`);
+}
+
+// sortAllDicts();
